f29449ff96ff35be0e2b5097f1dcbdc28fd83c59
Samson343 <sam.ban@gmail.com>
2022-12-17 15:19:38 -0700

    feature is working!
    needs some polish but the basics are there

--- src/Featured.js
+++ src/Featured.js
@@ -1,11 +1,28 @@
 import React from "react";
+import styles from './Featured.module.css'
 
 
-const Featured = ({ clickedPost }) => {
+const Featured = ({ clickedPost, setShouldFeature }) => {
     return (
-        <div>
+        <main className = {styles.main}>
+            <div className={styles.postCard} onClick={(e) => {
 
-        </div>
+            }
+            }>
+                <h5 className={styles.title}>{clickedPost.title[0].toUpperCase() + clickedPost.title.slice(1)}</h5>
+                <p className={styles.description}>{clickedPost.description}</p>
+                <p className={styles.price}>Price: {clickedPost.price}</p>
+                <p className={styles.seller}>Seller: {clickedPost.author.username}</p>
+
+                <button className= {styles.backButton} onClick = {() => {
+                        setShouldFeature (false)
+                        
+                   }
+                }
+                
+                >back</button>
+            </div>
+        </main>
     )
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { gatherPosts } from './apiIndex'
 import styles from './Main.module.css'
+import Featured from './Featured'
 
 export default function Main ({ setPosts, posts, token, isAuthenticated}) {
+
+    const [shouldFeature, setShouldFeature] = useState(false)
+    const [featureKey, setFeatureKey] = useState('')
 
     useEffect(() => {
         gatherPosts()
@@ -17,12 +21,25 @@ export default function Main ({ setPosts, posts, token, isAuthenticated}) {
 
     }, [])
 
+
     return (
+        shouldFeature ?
+        <Featured 
+            clickedPost = {featureKey}
+            setShouldFeature = {setShouldFeature}
+        />
+        :
         <div className={styles.main}>
             {
                 posts.map((elem, index) => (
                         
-                    <div key = {index} className = {styles.postCards}>
+                    <div key = {index} className = {styles.postCards} onClick = { (e) => {
+                        setShouldFeature(true)
+                        setFeatureKey(elem)
+                        console.log("this is the elem", elem)
+
+                      }
+                    }>
                         <h5 className= {styles.title}>{elem.title[0].toUpperCase() + elem.title.slice(1)}</h5>
                         <p className= {styles.description}>{elem.description}</p>
                         <p className= {styles.price}>Price: {elem.price}</p>
@@ -31,6 +48,8 @@ export default function Main ({ setPosts, posts, token, isAuthenticated}) {
                 )  
             )
             }
-        </div>
+        </div> 
+        
+        
     )
 }

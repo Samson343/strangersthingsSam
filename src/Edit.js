3df6ef40df2204fb2f84068cee0067668f47bc97
Samson343 <sam.ban@gmail.com>
2022-12-30 10:54:01 -0700

    edit form mostly styled
    values are grabbed, just need to hook it up to the api call next

--- src/Edit.js
+++ src/Edit.js
@@ -8,7 +8,7 @@ const Edit = ({ post, token, setRenderEdit }) => {
     const [editTitle, setEditTitle] = useState(false)
     const [editDesc, setEditDesc] = useState(false)
     const [editPrice, setEditPrice] = useState(false)
-    const [editLocation, setEditLoaction] = useState(false)
+    const [editLocation, setEditLocation] = useState(false)
     const [editDelivery, setEditDelivery] = useState (false)
 
     const [titleUpdate, setTitleUpdate] = useState ('')
@@ -31,30 +31,77 @@ const Edit = ({ post, token, setRenderEdit }) => {
                         }}>...</button>
                     </h5>
                     : <div>
-                        <input className = {styles.editInput} placeholder={post.title[0].toUpperCase() + post.title.slice(1)}></input>
+                        <input className = {styles.editInput} placeholder={post.title[0].toUpperCase() + post.title.slice(1)} value = {titleUpdate} onChange = {(e) => {
+                            setTitleUpdate(e.target.value)
+                        }}></input>
+                        <button className={styles.editButtons} onClick={() => {
+                            setEditTitle(false)
+                        }}>cancel</button>
                       </div>
                     }
-                        
+                    {  !editDesc ?   
                     <p className={styles.description}>{post.description} &nbsp;
                         <button className={styles.editButtons} onClick={() => {
                             setEditDesc(true)
                         }}>...</button>
                     </p>
+                       : <div>
+                            <input placeholder={post.description} className = {styles.editInput} value = {descUpdate} onChange = {(e) => {
+                            setDescUpdate(e.target.value)
+                        }}></input>
+                            <button className={styles.editButtons} onClick={() => {
+                            setEditDesc(false)
+                        }}>cancel</button>
+                        </div> 
+                    }
+                    {
+                        !editPrice ?
                     <p className={styles.price}>Price: {post.price} &nbsp;
                     <button className={styles.editButtons} onClick={() => {
                             setEditPrice(true)
                         }}>...</button>
                     </p>
-                    <p > <span className={styles.spans}>Location: &nbsp; </span>{post.location.split("[").join('').split(']').join('')} &nbsp;
+                    : <div>
+                    <input placeholder={post.price} className = {styles.editInput} value = {priceUpdate} onChange = {(e) => {
+                            setPriceUpdate(e.target.value)
+                        }}></input>
                     <button className={styles.editButtons} onClick={() => {
-                            setEditLoaction(true)
+                    setEditPrice(false)
+                    }}>cancel</button>
+                </div> 
+                    }
+                    { !editLocation ?
+                    <p className= {styles.location}> <span className={styles.spans}>Location: &nbsp; </span>{post.location.split("[").join('').split(']').join('')} &nbsp;
+                    <button className={styles.editButtons} onClick={() => {
+                            setEditLocation(true)
                         }}>...</button>
                     </p>
-                    <p><span className={styles.spans}>Willing to deliver: &nbsp;</span> &nbsp;
+                      : <div>
+                      <input placeholder={post.location.split("[").join('').split(']').join('')} className = {styles.editInput} value = {locationUpdate} onChange = {(e) => {
+                            setLocationUpdate(e.target.value)
+                        }}></input>
+                      <button className={styles.editButtons} onClick={() => {
+                      setEditLocation(false)
+                      }}>cancel</button>
+                  </div> 
+                    }
+                    { 
+                        !editDelivery ?
+                    <p className= {styles.location}><span className={styles.spans}>Willing to deliver: &nbsp;</span> &nbsp;
                     <button className={styles.editButtons} onClick={() => {
                             setEditDelivery(true)
                         }}>...</button>
                     </p>
+                        : <div>
+                            <p className = {styles.spans}> Willing to deliver?
+                                <input type="checkbox" name = "willDeliver" className = {styles.editInput}></input>
+                           <button className={styles.editButtons} onClick={() => {
+                              setEditDelivery(false)
+                              }}>cancel</button>
+                            </p>
+                         </div> 
+                    }
+                    
                     <p><span className={styles.spans}> Date Created: &nbsp;</span> {
                         post.updatedAt
                             .split('')
@@ -65,11 +112,7 @@ const Edit = ({ post, token, setRenderEdit }) => {
                             .slice(0, 4)
                             .join('')
                     } &nbsp;
-                    <button className={styles.editButtons} onClick={() => {
-
-                        }}>...</button>
                     </p>
-
 
                     <div className={styles.buttonWrapper}>
                         <button className={styles.backButton} onClick = {() => {
